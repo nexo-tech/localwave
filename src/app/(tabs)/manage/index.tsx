d6c79670175ce3c8b41179b8799189f25713a06b
crystalboxes <crystalboxesgfx@gmail.com>
2025-01-09 00:11:17 +0100

    add icloud connection

--- src/app/(tabs)/manage/index.tsx
+++ src/app/(tabs)/manage/index.tsx
@@ -7,15 +7,31 @@ import { useNavigationSearch } from "@/hooks/useNavigationSearch";
 import { useFavorites } from "@/store/library";
 import { defaultStyles } from "@/styles";
 import { useMemo } from "react";
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View, Text, TouchableOpacity, Platform } from "react-native";
+import { CloudStorage, CloudStorageProvider, useIsCloudAvailable } from 'react-native-cloud-storage';
 
+CloudStorage.setProvider(
+	Platform.select({
+		ios: CloudStorageProvider.ICloud,
+		default: CloudStorageProvider.ICloud,
+	})
+);
 const ManageScreen = () => {
+	const runMyLogin = () => {
+		console.log("running my app")
+	}
+	const isCloudAvailable = useIsCloudAvailable()
 
 	return (
 		<View style={defaultStyles.container}>
 			<View style={{ paddingTop: 150, width: "100%", flex: 1, height: "100%", }}>
 				<View style={{ padding: 10 }}>
-					<Text style={defaultStyles.text}>Helsdadlo world</Text>
+					<Text style={defaultStyles.text}>Cloud available: {isCloudAvailable ? "true" : "false"}</Text>
+					<TouchableOpacity onPress={() => {
+						runMyLogin();
+					}} style={{ padding: 20 }}>
+						<Text style={defaultStyles.text}>Run</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
 		</View>
